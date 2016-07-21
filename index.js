ace5ac079d370a1745b92b5aca4e6a9af3de7ec5
Jordan Klassen <jordan@klassen.me.uk>
2016-07-20 18:35:29 -0700

    wip promise checks

--- index.js
+++ index.js
@@ -2,11 +2,16 @@
 
 module.exports = {
   extends: 'xo-space',
+
   rules: {
+    // overrides
     'curly': [2, 'multi-line', 'consistent'],
     'brace-style': [2, '1tbs', {allowSingleLine: true}],
     'eqeqeq': [2, 'smart'],
     'no-eq-null': [0],
-    'no-use-before-define': [1]
+    'no-use-before-define': [1],
+    // additional rules
+    'promise/param-names': [2],
+    'promise/catch-or-return': [2]
   }
 };
